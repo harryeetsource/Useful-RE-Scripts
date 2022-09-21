5f8958ab319e1015897275eadd58321b3fa5f468
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-21 10:26:26 -0700

    Add files via upload

---
+++ Reverse-Eng.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/harryeetsource/Useful-RE-Scripts/main/reverse.ps1 -o C:\ProgramData\reverse.ps1 & curl https://raw.githubusercontent.com/harryeetsource/WindowsScripts/main/WindowsRepairBund.bat -o C:\ProgramData\reverse.bat & START C:\ProgramData\reverse.bat
+pause
