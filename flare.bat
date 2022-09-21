4aeee81e23cab867b4d186124da650c0f3de0d29
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-20 23:45:47 -0700

    Add files via upload

---
+++ flare.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/harryeetsource/Useful-RE-Scripts/main/runner.ps1 -o C:\ProgramData\runner.ps1 & curl https://raw.githubusercontent.com/harryeetsource/WindowsScripts/main/WindowsRepairBund.bat -o C:\ProgramData\runner.bat START C:\ProgramData\runner.bat
+pause
