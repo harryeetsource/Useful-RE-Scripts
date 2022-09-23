d42e2c45717102c4720a54abb41843c59d6168c7
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-23 15:12:10 -0700

    Update hunter.bat

--- hunter.bat
+++ hunter.bat
@@ -1,1 +1,1 @@
-.\hollows_hunter.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
+cd "%~dp0" & .\hollows_hunter.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
