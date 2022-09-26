0028b814ae17592fa07e7d12247be1f715faf6f1
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-26 16:09:03 -0700

    Update hunter.bat

--- hunter.bat
+++ hunter.bat
@@ -1,1 +1,1 @@
-.\hollows_hunter.exe /pname svchost.exe;explorer.exe;dllhost.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
+.\hollows_hunter.exe /pname svchost.exe;explorer.exe;dllhost.exe;winlogon.exe;runtimebroker.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
