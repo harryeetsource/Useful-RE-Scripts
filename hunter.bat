aa7fe6976dcffd956683154866c065c4ab5e5603
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-26 17:17:03 -0700

    Add files via upload

--- hunter.bat
+++ hunter.bat
@@ -1,1 +1,2 @@
-.\hollows_hunter.exe /pname svchost.exe;explorer.exe;dllhost.exe;winlogon.exe;runtimebroker.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
+cd %~dp0 & .\hollows_hunter.exe /pname svchost.exe;explorer.exe;dllhost.exe;winlogon.exe;runtimebroker.exe /cache /loop /refl /data 3 /shellc /threads /hooks /iat 3 /minidmp
+pause
