5d4943b043d82697b253f73340a14ae10ebecdbe
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-03-03 11:55:25 -0800

    Update xor.ps1

--- xor.ps1
+++ xor.ps1
@@ -1,5 +1,15 @@
-[byte[]] $x = get-content -encoding byte -path file.exe
+$key = 0xFF
 
-for ($x = 0; $x -lt $var_code.Count; $x++) {
-    $var_code[$x] = $var_code[$x] -bxor 35
+# Get the list of files in the current directory
+$fileList = Get-ChildItem -File
+
+# Loop through each file and decrypt it using XOR
+foreach ($file in $fileList) {
+    $content = [System.IO.File]::ReadAllBytes($file.FullName)
+    for ($i = 0; $i -lt $content.Length; $i++) {
+        $content[$i] = $content[$i] -bxor $key
+    }
+
+    # Write the decrypted content back to the file
+    [System.IO.File]::WriteAllBytes($file.FullName, $content)
 }
